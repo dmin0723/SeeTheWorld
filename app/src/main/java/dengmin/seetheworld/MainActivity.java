4f3b75c4f5608429a1b45f7e3d5c9e24b3591b96
coder-dmin <gzdengmin@126.com>
2016-04-30 23:12:19 +0800

    Initial commit

---
+++ app/src/main/java/dengmin/seetheworld/MainActivity.java
@@ -0,0 +1,13 @@
+package dengmin.seetheworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
