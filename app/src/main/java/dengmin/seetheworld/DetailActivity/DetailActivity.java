c80c37ccea93e8125a12a8c3c6344363d4d7e1ff
coder-dmin <gzdengmin@126.com>
2016-05-01 16:05:38 +0800

    完成基本的布局

---
+++ app/src/main/java/dengmin/seetheworld/DetailActivity/DetailActivity.java
@@ -0,0 +1,10 @@
+package dengmin.seetheworld.DetailActivity;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by dmin on 2016/5/1.
+ */
+public class DetailActivity extends AppCompatActivity {
+
+}
