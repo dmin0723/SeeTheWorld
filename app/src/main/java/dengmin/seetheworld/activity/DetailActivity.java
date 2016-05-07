b4a788d220f23af58a50b79e995695322a37c99c
coder-dmin <gzdengmin@126.com>
2016-05-07 18:53:52 +0800

    增加了个人信息部分

--- app/src/main/java/dengmin/seetheworld/activity/DetailActivity.java
+++ app/src/main/java/dengmin/seetheworld/activity/DetailActivity.java
@@ -1,10 +1,67 @@
 package dengmin.seetheworld.activity;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.widget.ProgressBar;
+
+import dengmin.seetheworld.R;
+import dengmin.seetheworld.bean.Constant;
+import dengmin.seetheworld.bean.NewsItem;
 
 /**
  * Created by dmin on 2016/5/1.
  */
 public class DetailActivity extends AppCompatActivity {
+    private WebView webView;
+    private Toolbar toolbar;
+    private ProgressBar progressBar;
+    private NewsItem newsItem;
 
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        //初始化视图
+        initView();
+    }
+
+    private void initView() {
+        webView = (WebView) findViewById(R.id.web_view);
+        progressBar = (ProgressBar) findViewById(R.id.progress_bar);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        //添加标题栏
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setHomeButtonEnabled(true);//左上角可点击
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);//左上角有图标显示
+
+        //反序列化，取出数据 使用Intent来传递对象
+        Bundle bundle = getIntent().getExtras();
+        newsItem = (NewsItem) bundle.getSerializable(Constant.NEWS_ITEM);
+
+        //显示页面的进度条
+        webView.setWebChromeClient(new WebChromeClient(){
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                if(newProgress == 100){
+                    progressBar.setVisibility(View.INVISIBLE);
+                }else{
+                    if(progressBar.getVisibility() == View.INVISIBLE){
+                        progressBar.setVisibility(View.VISIBLE);
+                    }
+                    progressBar.setProgress(newProgress);
+                }
+                super.onProgressChanged(view, newProgress);
+            }
+        });
+
+        //页面下载的地址
+        webView.loadUrl(newsItem.getUrl());
+    }
 }
--- app/src/main/java/dengmin/seetheworld/activity/Personal.java
+++ app/src/main/java/dengmin/seetheworld/activity/Personal.java
@@ -1,14 +1,19 @@
 package dengmin.seetheworld.activity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import dengmin.seetheworld.R;
 import dengmin.seetheworld.bean.User;
+import dengmin.seetheworld.biz.UserProxy;
+import dengmin.seetheworld.views.ChooseDialog;
+import dengmin.seetheworld.views.EditTextDialog;
 import dengmin.seetheworld.views.LoadingDialog;
 
 /**
@@ -32,15 +37,72 @@ public class Personal extends AppCompatActivity implements View.OnClickListener{
         //初始化
         initViews();
 
+        //获取当前用户名
+        user = UserProxy.getCurrentUser(this);
+        if(user != null){
+            textView_UserName.setText(user.getUsername());//使用了bomb
+        }
 
-
+        //设置点击事件
+        setListener();
     }
 
+    //设置点击事件
+    private void setListener() {
+        button_PetName.setOnClickListener(this);
+        button_Sex.setOnClickListener(this);
+        button_Signature.setOnClickListener(this);
+        button_Logout.setOnClickListener(this);
+    }
+
+    //初始化页面
     private void initViews() {
+        //添加toolbar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(R.string.personal);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //实例化各个控件
+        loadingDialog = new LoadingDialog();
+        textView_UserName = (TextView) findViewById(R.id.tv_username);
+        button_PetName = (Button) findViewById(R.id.btn_petname);
+        button_Sex = (Button) findViewById(R.id.btn_sex);
+        button_Signature = (Button) findViewById(R.id.btn_signature);
+        button_Logout = (Button) findViewById(R.id.btn_logout);
+        textView_PetName = (TextView) findViewById(R.id.tv_petname);
+        textView_Sex = (TextView) findViewById(R.id.tv_sex);
     }
 
     @Override
     public void onClick(View v) {
+        EditTextDialog editTextDialog = new EditTextDialog();
 
+        switch (v.getId()){
+            //点击昵称
+            case R.id.btn_petname:
+                editTextDialog.setParams(textView_PetName.getText().toString(),
+                        true,20);
+//                editTextDialog.
+                break;
+
+            //点击性别
+            case R.id.tv_sex:
+                final String[] string = {"男","女"};
+                ChooseDialog chooseDialog = new ChooseDialog();
+                chooseDialog.setParams(null, string, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //提示
+                        loadingDialog.show(getFragmentManager(),"loading");
+
+                        //更改性别是否成功
+
+
+
+                    }
+                });
+        }
     }
 }
